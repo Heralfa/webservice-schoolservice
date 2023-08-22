92f6fa103003d202882bcff5edcac3dcc2d3c850
Tomas Alvarez <l.aalt011003@itses.edu.mx>
2023-08-22 09:46:18 -0600

    Ultimos servicios2

--- conexion.php
+++ conexion.php
@@ -1,18 +1,8 @@
 <?php
-
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
-}
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST");
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    exit(0);
-}
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json; charset=utf-8");
+header("Access-Control-Allow-Headers: Authorization, Cache-Control, Content-Type, X-Requested-With");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, INSERT, DELETE, insertUser, deleteUser, PATCH, PUT, HEAD");
 
 class Conexion
 {
